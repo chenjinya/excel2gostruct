11a09aace7e6656de092d2aefffd65833767668d
chenjinya <chenjinya@zhihu.com>
2023-08-31 15:43:53 +0800

    feat: add excel2gostruct

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # excel2gostruct
+
+Convert excel datasets to golang struct 
+
+
+```golang
+type Strategy struct {
+	Number      string `json:"number"`
+	Stage       string `json:"stage"`
+	Description string `json:"description"`
+	Strategy    string `json:"strategy"`
+}
+
+func main() {
+	// 打开 Excel 文件
+	targets := []Strategy{}
+	err := ConvertExcelToGo("stage.xlsx", &targets)
+	if err != nil {
+		panic(err)
+	}
+	//println(utils.MustMarshalToString(targets))
+}
+
+```
---
+++ excel2gostruct.go
@@ -0,0 +1,64 @@
+package excel2gostruct
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/360EntSecGroup-Skylar/excelize"
+	"strconv"
+)
+
+func getColumnName(n int) string {
+	columnName := ""
+	for n > 0 {
+		n--
+		columnName = string('A'+n%26) + columnName
+		n /= 26
+	}
+	return columnName
+}
+
+func ConvertExcelToGo(filepath string, targets interface{}, sheetIndex int) (err error) {
+	if sheetIndex == 0 {
+		sheetIndex = 1
+	}
+	if filepath == "" {
+		return errors.New("filepath is empty")
+	}
+	if targets == nil {
+		return errors.New("targets is nil")
+	}
+	f, err := excelize.OpenFile(filepath)
+	if err != nil {
+		return err
+	}
+	var rowIndexToValue = make(map[string]string)
+	var datasets []map[string]string
+	sheetName := f.GetSheetName(sheetIndex)
+	rows := f.GetRows(sheetName)
+	for i, row := range rows {
+		rawData := make(map[string]string)
+		for j := range row {
+			colName := getColumnName(j + 1)
+			axis := colName + strconv.Itoa(i+1)
+			if i == 0 {
+				name := f.GetCellValue(sheetName, axis)
+				rowIndexToValue[colName] = name
+			} else {
+				rawData[rowIndexToValue[colName]] = f.GetCellValue(sheetName, axis)
+			}
+		}
+		if i > 0 {
+			datasets = append(datasets, rawData)
+		}
+
+	}
+	ms, err := json.Marshal(datasets)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(ms, &targets)
+	if err != nil {
+		return err
+	}
+	return nil
+}
